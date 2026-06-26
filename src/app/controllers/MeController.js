ef51b10d05fc82f6316d6be12ab2a9d64bb6d5dc
buiduc9918 <buiduc9918>
2026-06-26 16:03:18 +0700

    sua lại giao dien 2

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,18 +2,22 @@ const Course = require("../model/Course");
 const { multipleMongooseToObject } = require("../../util/mongoose");
 
 class MeController {
-   // [GET] /me/store
+  // [GET] /me/news
+  index(req, res, next) {
+    res.render("news");
+  }
+  // [GET] /me/store
   async stored(req, res, next) {
-     try {
-          const filter = {};
-          const courses = multipleMongooseToObject(await Course.find(filter));
-          if (!courses || courses.length === 0) {
-            res.render("me/stored", { courses: courses });
-          }
-          res.render("me/stored", { courses: courses });
-        } catch (error) {
-          next(error);
-        }
+    try {
+      const filter = { deletedAt:null };
+      const courses = multipleMongooseToObject(await Course.find(filter));
+      if (!courses || courses.length === 0) {
+        res.render("me/stored", { courses: courses });
+      }
+      res.render("me/stored", { courses: courses });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- src/routes/me.js
+++ src/routes/me.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const meController = require('../app/controllers/MeController');
 
+router.get('/news', meController.index);
+
 router.get('', meController.stored);
 
 module.exports = router;
